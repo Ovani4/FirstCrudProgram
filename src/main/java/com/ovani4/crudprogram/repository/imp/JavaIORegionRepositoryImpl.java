700915b99d9cfc3a492d303f873c746db4213174
Ovani4 <ovani4@ya.ru>
2021-03-12 21:38:24 +0300

    change methods in class JavaIORegionRepositoryImpl

--- src/main/java/com/ovani4/crudprogram/repository/imp/JavaIORegionRepositoryImpl.java
+++ src/main/java/com/ovani4/crudprogram/repository/imp/JavaIORegionRepositoryImpl.java
@@ -19,6 +19,7 @@ public class JavaIORegionRepositoryImpl implements RegionRepository {
 
     @Override
     public Region getById(Integer integer) { //Иначе не додумался как из потока объект вытащить
+        //попробовать вытащить Object  и привести его к Region...
 
         Region[] regions = (Region[]) getListFromFile(FILE_PATH_REGIONS).stream().
                 filter(r -> r.equals(integer)).toArray();
@@ -45,15 +46,31 @@ public class JavaIORegionRepositoryImpl implements RegionRepository {
 
     @Override
     public Region update(Region region) {
-
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH_REGIONS))) {
+            getListFromFile(FILE_PATH_REGIONS).stream().
+                    filter(region1 -> region.getId().equals(region)).forEach(region1 -> {
+                        //Еще не придумал, как зменить объект в потоке)
+            });
+        }catch (IOException e){
+            System.err.println("error in update region");
+        }
         return region;
     }
 
     @Override
-    public void deleteById(Integer integer) { //помоему он просто удалит из потока, список не изменится
-        getListFromFile(FILE_PATH_REGIONS).stream().
-                filter(region -> region.getId().equals(integer)).
-                forEach(region -> getAll().remove(region));
+    public void deleteById(Integer integer) {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH_REGIONS))) {
+            getListFromFile(FILE_PATH_REGIONS).stream().
+                    filter(region -> region.getId() != integer).forEach(region -> {
+                try {
+                    bw.write(region.getId().toString() + " " + region.getName() + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }catch (IOException e){
+            System.err.println("error in delete region by ID");
+        }
     }
 
     public List<Region> getListFromFile(String filePath) {
